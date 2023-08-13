67d6b7994b83bc96f0f90736c5b72954616eb19a
Nikhil1O1 <technikue2.0@gmail.com>
2023-08-13 17:51:58 +0530

    v 0.6 defined schema and model
    can now add users via api

--- mernapp/backend/db.js
+++ mernapp/backend/db.js
@@ -1,19 +1,34 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://gofood:taKGJr2k5ojxkl98@cluster0.bgpaa3e.mongodb.net/?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://gofood:taKGJr2k5ojxkl98@cluster0.bgpaa3e.mongodb.net/gofoodmern?retryWrites=true&w=majority'
 
 process.on('uncaughtException', err => {
-    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-    console.log(err.name, err.message);
-    process.exit(1);
-  });
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
-const mongoDB = async() => {
-    await mongoose
+// async function called(){
+//   console.log('Connection successful!');
+//   const fetched_data = await mongoose.connection.db.collection("food_items");
+//   await fetched_data.find({}).toArray(function(err, data){
+//     // if(err) console.log(err);
+//     // else console.log(data);
+//     console.log(data)})
+// };
+
+async function called() {
+  console.log('Connection successful!');
+  const FoodCategory = mongoose.model('food_items', new mongoose.Schema({}));
+  const data = await FoodCategory.find().exec();
+  // console.log(data);
+  }
+
+const mongoDB = async () => {
+  await mongoose
     .connect(mongoURI, {
       useNewUrlParser: true
     })
-    .then(() => console.log('DB connection successful!'));
+    .then(() => called())
 }
-
 
 module.exports = mongoDB;
